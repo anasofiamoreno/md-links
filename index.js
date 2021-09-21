c28909bf4c01224b30a7b2148327bb29bdf08bd9
anasofiamoreno <84933610+anasofiamoreno@users.noreply.github.com>
2021-09-21 18:46:48 -0500

    Valida si las direcciones estan activas y muestar los resultados en la consola

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
-let action = require('./script');
+let mdLinks = require('./script');
+let option = 0;
 
-action(process.argv[2], process.argv[3]);
+switch(process.argv[3]){
+    case '--validate': option = { validate: true };
+    break;
+    default: option = { validate: false };
+}
+
+mdLinks(process.argv[2], option)
+  .then( (message) => {
+      //console.log(message);
+      if(option.validate){
+        message.forEach(element => {
+          console.log(element.file, element.href, element.ok, element.status, element.text);
+        });
+      }
+      else{
+        message.forEach(element => {
+          console.log(element.file, element.href, element.text);
+        });
+      }
+      
+  });
--- script.js
+++ script.js
@@ -1,41 +1,83 @@
 module.exports = function mdLinks (path, options) {
-  console.log('path : ' + path);
-  console.log('options : ' + options);
+  return new Promise( (resolve1, reject1) => {
+    var XMLHttpRequest = require('xhr2');
   let readline = require('readline');
   let fs  = require('fs');
   let listUrls = [];
+  let contListUrls = 0;
+  let lines = [];
   let numLinea = 1;
- 
+  let response = [];
+  
+ /*
   fs.readFile(path, 'utf-8', (err, data) => {fnReadReady(data)});
   
-
   function fnReadReady(data, error){
- 
-    //console.log(data);
-    //let ss = fs.createReadStream(path);
-    //console.log(ss.on());
+    console.log(data);
+    let ss = fs.createReadStream(path);
+    console.log(ss.on());
+  }
+  */
 
-    let lector = readline.createInterface({
-      input: fs.createReadStream(path),
-      completer: fnterminado(),
+  let lector = readline.createInterface({
+    input: fs.createReadStream(path),
+  });
+
+  lector.on('line',line => {
+    lines.push(line)
+  }).on('close', () => {
+    fnValite(lines)
+    .then((message) => {resolve1(message)})
+  });
+
+  function fnValite(linesToEvaluate){
+    return new Promise( (resolve2,reject2) => {
+      linesToEvaluate.map( (element) => {
+        if(element.includes('https')){
+          //console.log('linea ' + numLinea, element);
+          const ss = element.slice(element.indexOf('https')).split(')')
+          //console.log('Url:',ss[0]);
+          listUrls.push(ss[0]);
+          //request.addEventListener('load',function() {console.log(request.status, request._url.href)});
+          let request = new XMLHttpRequest();
+          request.open('HEAD', ss[0], true);
+          request.onreadystatechange = function (e) {
+            if(request.readyState == 4){
+              contListUrls++;
+              //console.log('yes', request.status, request._url.href);
+              let ok = new String;
+              if(request.status == 200){
+                ok = 'ok';
+              }
+              else{
+                ok = 'fail';
+              }
+
+              if(options.validate){
+                response.push({href:request._url.href, text: "empty", file: path, status:request.status, ok: ok});
+              }
+              else{
+                response.push({href:request._url.href, text: "empty", file: path});
+              }
+              
+              //console.log(contListUrls);
+              //console.log(listUrls.length);
+              if(contListUrls == listUrls.length ){
+                resolve2(response);
+              }
+              
+              
+            }
+            
+          }
+          request.send(null)
+        }
+        numLinea++;
+      });
     });
-
-    lector.on('line', linea => {
-      if(linea.includes('http')){
-        console.log('linea ' + numLinea, linea);
-        const ss = linea.slice(linea.indexOf('http')).split(')')
-        console.log('Url:',ss[0]);
-        listUrls.push(ss[0]);
-        console.log("");
-      }
-     numLinea++;
-    });
-
-    
-    function fnterminado(){
-      console.log("terminado");
-    }
   }
 
+  })
+  
 };
 
