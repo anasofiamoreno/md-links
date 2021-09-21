99134d337724b00856f66671c69b4222c18877d7
anasofiamoreno <84933610+anasofiamoreno@users.noreply.github.com>
2021-09-21 12:04:45 -0500

    Filtra las urls y muestra una lista de las urls encontradas.

--- script.js
+++ script.js
@@ -3,33 +3,39 @@ module.exports = function mdLinks (path, options) {
   console.log('options : ' + options);
   let readline = require('readline');
   let fs  = require('fs');
-  let lines = [];
+  let listUrls = [];
+  let numLinea = 1;
  
   fs.readFile(path, 'utf-8', (err, data) => {fnReadReady(data)});
   
 
   function fnReadReady(data, error){
  
-    console.log(data);
+    //console.log(data);
     //let ss = fs.createReadStream(path);
     //console.log(ss.on());
 
     let lector = readline.createInterface({
-         input: fs.createReadStream(path)
+      input: fs.createReadStream(path),
+      completer: fnterminado(),
     });
 
     lector.on('line', linea => {
-     console.log('linea', linea);
+      if(linea.includes('http')){
+        console.log('linea ' + numLinea, linea);
+        const ss = linea.slice(linea.indexOf('http')).split(')')
+        console.log('Url:',ss[0]);
+        listUrls.push(ss[0]);
+        console.log("");
+      }
+     numLinea++;
     });
 
-
-
-
-
-
+    
+    function fnterminado(){
+      console.log("terminado");
+    }
   }
-
-
 
 };
 
